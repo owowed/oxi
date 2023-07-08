99a6f07ce08e993a957696883f0776334faf18ed
owowed <island@owowed.moe>
2023-07-08 11:55:56 +0700

    feat: add format.ts containing string formatting utils and escaping regex

---
+++ src/format.ts
@@ -0,0 +1,23 @@
+
+export interface FormatStringOptions {
+    subst: {
+        format: string;
+        var: string;
+    }
+}
+
+export function escapeRegExp(text: string) {
+    return text.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+export function formatString(
+        text: string,
+        dict: Record<string, any>,
+        { subst = { format: "${{ | }}", var: "|" } }: Partial<FormatStringOptions> = {}): string
+{
+    const substRegex = subst.format
+        .split(subst.var)
+        .map(escapeRegExp)
+        .join(String.raw`([$\w\d-_.: ]+)`);
+    return text.replace(new RegExp(substRegex, "g"), (_, varKey) => dict[varKey]?.toString());
+}
